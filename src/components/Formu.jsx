818f00a5c068fe1378697371a4340da7971c5133
VioletaRS26 <svioleta1002@gmail.com>
2023-03-21 17:37:23 -0300

    Atualização do codigo Login e inicio do form

--- src/components/Formu.jsx
+++ src/components/Formu.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function Form() {
     return(
-        <div>
-            <div>1</div>
-            <div>&nbsp</div>
-            <div>2</div>
-            <div>&nbsp</div>
-            <div>4</div>
-            <div>&nbsp</div>
-            <div>5</div>
+        <div className="flex flex-row bg-red-200 justify-center items-center p-4">
+            <div className="flex flex-row bg-white rounded-full p-3 px-5">1</div>
+            <div className="bg-white px-4 ">&nbsp;</div>
+            <div className="flex flex-row bg-white rounded-full p-3 px-5">2</div>
+            <div className="bg-white px-4 ">&nbsp;</div>
+            <div className="flex flex-row bg-white rounded-full p-3 px-5">3</div>
+            <div className="bg-white px-4 rounded-1xl ">&nbsp;</div>
+            <div className="flex flex-row bg-white rounded-full p-3 px-5">4</div>
 
         </div>
     )
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,18 +15,20 @@ function Login(){
     }
 
     return(
-        <div className="flex flex-col items-center">
-            <div className="m-14">
-                <img src="./logo.png" alt="Logo" />
-            </div>
-            <h1 className="text-3xl font-bold">Entre com sua conta</h1><br></br>
-            <form className="py-9 flex-col items-end" onSubmit={handleSubmit} >
-                <div className="flex flex-col">
-                    <input type="text" name="username" value={inputs.username || ""} onChange={handleChange} placeholder="Name" className="b-2  text-justify focus:border-black border-2 border-gray-300 rounded-2xl py-1 px-9"/>
-                    <input type="text" name="inputs.senha" value={inputs.senha || ""} onChange={handleChange} placeholder="Senha" className="b-2  text-justify my-3 focus:border-black border-2 border-gray-300 rounded-2xl py-1 px-9"/>
-                    <input type="submit" className="b-2 my-3"/>
+        <div className="flex flex-col ba-white">
+            <div className="flex flex-col items-center justify-center  w-15 rounded-xl">
+                <div className="m-14">
+                    <img src="./logo.png" alt="Logo" />
                 </div>
-            </form>
+                <h1 className="text-3xl font-bold">Entre com sua conta</h1><br></br>
+                <form className=" flex-col items-center m-2 " onSubmit={handleSubmit} >
+                        <div className="flex flex-col">
+                            <input type="text" name="username" value={inputs.username || ""} onChange={handleChange} placeholder="Name" className="b-2  text-justify focus:border-black border-2 border-gray-300 rounded-2xl py-1 px-9"/>
+                            <input type="text" name="inputs.senha" value={inputs.senha || ""} onChange={handleChange} placeholder="Senha" className="b-2  text-justify my-3 focus:border-black border-2 border-gray-300 rounded-2xl py-1 px-9"/>
+                            <input type="submit" className=" b-2 px-4 bg-slate-400 "/>
+                        </div>
+                    </form>
+            </div>
         </div>
 
     )
